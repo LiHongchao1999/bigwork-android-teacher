5baa37f41ae7c732d192426e808d9224e1a1c268
张嘉豪 <44825512+zhanggahao@users.noreply.github.com>
2020-12-06 10:24:07 +0800

    1

---
+++ app/src/main/java/com/example/homeworkcorrectteacher/DetailHomeworkOrder.java
@@ -0,0 +1,14 @@
+package com.example.homeworkcorrectteacher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailHomeworkOrder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_homework_order);
+    }
+}

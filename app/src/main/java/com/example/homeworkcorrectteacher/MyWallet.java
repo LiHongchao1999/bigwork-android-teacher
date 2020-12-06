2e3be3c9ae4c4873e5bb0f2e740471a2b6cf937c
张嘉豪 <44825512+zhanggahao@users.noreply.github.com>
2020-12-06 10:51:33 +0800

    1

--- app/src/main/java/com/example/homeworkcorrectteacher/MyWallet.java
+++ app/src/main/java/com/example/homeworkcorrectteacher/MyWallet.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class DetailHomeworkOrder extends AppCompatActivity {
+public class MyWallet extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail_homework_order);
+        setContentView(R.layout.activity_my_wallet);
     }
 }
